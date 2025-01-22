638e6ecdec6097789cc80de5328858845a360148
Alex <past6867@gmail.com>
2025-01-22 12:08:37 -0500

    Día 2

---
+++ Pruebfinal.py
@@ -0,0 +1,6 @@
+nombre = str(input("Ingrese su nombre: "))
+ganancia = float(input("Ingrese su ganancia en ventas: "))
+
+resultado = round(ganancia * 13 / 100,2)
+print("Su salario mensual por comision es:", resultado)
+
---
+++ Redondeo.py
@@ -0,0 +1,21 @@
+print(round(90/7))
+
+
+valor = round(95.6666666666, 1)
+print(valor)
+
+
+valor = round(10/3,2)
+print(valor)
+
+valor = round(10.676767)
+print(valor)
+
+valor = round(5 ** 0.5, 4)
+print(valor)
+
+num1 = 10
+num2 = 25
+num1 = num2
+
+print(num1)
